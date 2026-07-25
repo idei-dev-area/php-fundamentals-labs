7ec863ef42631c6e31984dfe29f966311add9f9a
Baltazar Vivares <baltajvivares@gmail.com>
2026-07-25 20:52:14 -0300

    Baltazarvivares

--- lab01/ejercicio.php
+++ lab01/ejercicio.php
@@ -7,3 +7,39 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #1
+$alumnos = [
+    [
+        "nombre" => "Juan",
+        "edad" => 20,
+        "notas" => [8.0, 7.5, 6.0]
+    ],
+    [
+        "nombre" => "Savall",
+        "edad" => 21,
+        "notas" => [8.0, 7.5, 9.0]
+    ],
+    [
+        "nombre" => "Alejo",
+        "edad" => 18,
+        "notas" => [10.0, 10.0, 9.0]
+    ],
+    [
+        "nombre" => "Carlos",
+        "edad" => 20,
+        "notas" => [8.0, 6.0, 9.0]
+    ]
+];
+foreach ($alumnos as $alu){
+    $prom=0;
+    $cant=0;
+    foreach ($alu["notas "] as $nota){
+        $prom+=$nota;
+        $cant+=1;
+    }
+    $promedio=$prom/$cant;
+    if($promedio>=7){
+        echo "Alumno " . $alu["nombre"] . ": Aprobado con " . $promedio . "\n";
+    } else {
+        echo "Alumno " . $alu["nombre"] . ": Desaprobado\n";
+    }
+}
