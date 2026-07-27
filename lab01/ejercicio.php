b9b47d15c3cf1805ac80143386f72bac5a13d7ac
Rocio Heredia <rheredia@iinfo.unsj.edu.ar>
2026-07-27 18:25:30 -0300

    feat: completar ejercicio del laboratorio 01

--- lab01/ejercicio.php
+++ lab01/ejercicio.php
@@ -7,3 +7,20 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #1
+
+//Arreglo de alumnos con sus respectivas edades y notas
+$arregloAlumnos = [['nombre'=>'Juan', 'edad'=>24, 'notas'=>[9.0, 9.0, 7.5]],
+                    ['nombre'=>'María', 'edad'=>22, 'notas'=>[7.0, 8.5, 8.5]],
+                    ['nombre'=>'Pedro', 'edad'=>21, 'notas'=>[8.0, 6.5, 8.0]],
+                    ['nombre'=>'Ana', 'edad'=>23, 'notas'=>[6.5, 6.0, 5.0]]];
+
+
+foreach($arregloAlumnos as $alumno){
+    $promedio = array_sum($alumno['notas']) / count($alumno['notas']);
+
+    if ($promedio >= 7){
+        echo "Alumno " . $alumno['nombre'] . ": Aprobado con " . $promedio . "\n";
+    }else{
+        echo "Alumno " . $alumno['nombre'] . ": Desaprobado" . "\n";
+    }
+}
