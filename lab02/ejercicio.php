c3180ac3380438fd00e9d8e6cf05f5a5dccb209c
Baltazar Vivares <baltajvivares@gmail.com>
2026-07-27 17:36:04 -0300

    feat: implement calcularTotalViaticos function with error handling

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -7,3 +7,18 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #2
+function calcularTotalViaticos(int $dias, float $montoDiario, float $gastosExtras): float{
+    if ( $dias <= 0 or $montoDiario <=0 or $gastosExtras <=0){
+        throw new Exception("Valos ingresados tienen que ser mayores a 0");
+    }
+    $total=($dias*$montoDiario)+$gastosExtras;
+    return $total;
+}
+try {
+    $total = calcularTotalViaticos(-5, 1000.0, 500.0);
+    echo "Total de viáticos: $" . $total;
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+echo PHP_EOL . "El programa se ejecuta";
