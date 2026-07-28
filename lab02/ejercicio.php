b9f920d228918d218421f783d0272447d0790562
Mia Pastor <mpastor@iinfo.edu.com.ar>
2026-07-28 18:53:26 -0300

    feat: completar ejercicios del laboratorio 02

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -7,3 +7,17 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #2
+
+function calcularTotalViaticos(int $dias, float $montoDiario, float $gastosExtras): float {
+    if($dias<=0 || $montoDiario<=0 || $gastosExtras<=0){
+        throw new Exception("Los valores deben ser positivos.\n");
+    }else{
+        return ($dias*$montoDiario)+$gastosExtras;
+    }
+}
+
+try{
+    echo "El total de viáticos es: $" . number_format(calcularTotalViaticos(5, 100.5, 50.75), 2) . "\n";
+}catch (Exception $e) {
+    echo "Error: " . $e->getMessage();
+}
