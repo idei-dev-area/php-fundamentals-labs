cc74166d9d43a8031eb0aba4ab783bdc0da67c0a
Baltazar Vivares <baltajvivares@gmail.com>
2026-07-28 15:08:17 -0300

    feat: implement MiembroEquipo class with task completion and profile retrieval

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -7,3 +7,37 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #3
+class MiembroEquipo
+{
+    private string $nombre;
+    private string $rol;
+
+    public int $tareasCompletadas = 0;
+
+    public function __construct(string $nombre, string $rol)
+    {
+        $this->nombre = $nombre;
+        $this->rol = $rol;
+    }
+
+    public function completarTarea(): void
+    {
+        $this->tareasCompletadas++;
+    }
+
+    public function obtenerPerfil(): string
+    {
+        return "El miembro {$this->nombre} desempeña el rol de {$this->rol} y ha completado {$this->tareasCompletadas} laboratorios.";
+    }
+}
+
+$tl = new MiembroEquipo("Juan", "Technical Leader");
+$junior = new MiembroEquipo("Baltazar", "Junior");
+
+$tl->completarTarea();
+$tl->completarTarea();
+
+$junior->completarTarea();
+
+echo $tl->obtenerPerfil() . PHP_EOL;
+echo $junior->obtenerPerfil() . PHP_EOL;
