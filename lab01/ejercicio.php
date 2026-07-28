1555cbd351d12d958199758e862e2d9ae6ed6680
Facundo Gallardo <facundoggallardo@gmail.com>
2026-07-28 13:45:58 -0300

    feat: completar ejercicios del laboratiorio 01

--- lab01/ejercicio.php
+++ lab01/ejercicio.php
@@ -6,4 +6,26 @@ declare(strict_types=1);
 // Tema: Sintaxis y Estructuras de Control
 // ==========================================
 
-// Desarrolla aquí tu solución según el Issue #1
+$alumnos = [
+    ['nombre' => 'Martin', 'edad' => 23,  'notas' => [8.4, 9.1, 4.5]],
+    ['nombre' => 'Marcelo', 'edad' => 22, 'notas' => [9.0, 8.5, 9.5]],
+    ['nombre' => 'Ana', 'edad' => 24, 'notas' => [8.3, 4.5, 8.6]],
+    ['nombre' => 'Lucas', 'edad' => 19, 'notas' => [6.0, 5.5, 6.0]],
+    ['nombre' => 'Florencia', 'edad' => 21, 'notas' => [10.0, 7.7, 9.8]],
+    ['nombre' => 'Nicolas', 'edad' => 20, 'notas' => [7.0, 7.5, 8.0]],
+];
+
+foreach ($alumnos as $alumno) {
+  $sumaNotas = 0;
+  foreach ($alumno['notas'] as $nota) {
+    $sumaNotas += $nota;
+  }
+
+  $promedio = round($sumaNotas / count($alumno['notas']), 2);
+
+    if ($promedio >= 7) {
+      echo "Alumno {$alumno['nombre']}: Aprobado con {$promedio}\n";
+    } else {
+      echo "Alumno {$alumno['nombre']}: Desaprobado\n";
+    }
+}
