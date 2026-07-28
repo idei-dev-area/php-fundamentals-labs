d3fed5e95f9d825e9faf951cfe103fe48d2790a2
Mia Pastor <mpastor@iinfo.edu.com.ar>
2026-07-28 19:26:57 -0300

    feat: completar ejercicios del laboratorio 03

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -7,3 +7,43 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #3
+
+class MiembroEquipo{
+    private string $nombre;
+    private string $rol;
+    public int $tareasCompletadas=0;
+
+    function __construct(string $nombre, string $rol){
+        $this->nombre=$nombre;
+        $rolesValidos=["Junior", "Technical Leader"];
+        if(!in_array($rol, $rolesValidos)){
+            throw new Exception("Rol no válido.\n");
+        }else{
+            $this->rol=$rol;
+        }
+    }
+
+    function completarTarea(): void{
+        $this->tareasCompletadas++;
+    }
+
+    function obtenerPerfil(): string{
+        return "El miembro del equipo " . $this->nombre . " tiene rol de " . $this->rol . " y ha completado " . $this->tareasCompletadas . " tareas.\n";
+    }
+}
+
+try{
+    $miembro1 = new MiembroEquipo("Mia", "Junior");
+    $miembro1->completarTarea();
+    $miembro1->completarTarea();
+    echo $miembro1->obtenerPerfil();
+
+    $miembro2 = new MiembroEquipo("Alejo", "Technical Leader");
+    $miembro2->completarTarea();
+    echo $miembro2->obtenerPerfil();
+
+    $miembro3 = new MiembroEquipo("Nicolas", "Intern");
+}catch(Exception $e){
+    echo "Error: " . $e->getMessage();
+}
+
