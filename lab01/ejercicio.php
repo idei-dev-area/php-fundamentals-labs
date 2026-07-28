b779b869ae461e94e0b78ac52c685f83c3ebca98
AlejoSavall <asavall@iinfo.unsj.edu.ar>
2026-07-28 17:36:28 -0300

    feat: resolver laboratorio 01

--- lab01/ejercicio.php
+++ lab01/ejercicio.php
@@ -7,3 +7,36 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #1
+
+$alumnos=[
+    [
+        "nombre" => "Juan",
+        "edad" => 20,
+        "notas" => [8.0, 7.5, 9.0]
+    ],
+    [
+        "nombre" => "Alejo",
+        "edad" => 20,
+        "notas" => [8.0, 9.5, 10.0] 
+    ],
+    [
+        "nombre" => "Mia",
+        "edad" => 20,
+        "notas" => [9.0, 8.5, 6.5] 
+    ],
+    [
+        "nombre" => "Baltazar",
+        "edad" => 20,
+        "notas" => [8.0, 9.5, 6.0] 
+    ]
+];
+
+
+foreach ($alumnos as $alumno){
+    $promedio = array_sum($alumno['notas']) / count($alumno['notas']);
+    if ($promedio >= 7){
+        echo "Alumno " . $alumno["nombre"] . ": Aprobado con " . number_format($promedio, 2) . "\n";
+    }else{
+        echo "Alumno " . $alumno["nombre"] . ": Desaprobado" ."\n";
+    }
+}
