d396fb7d30784f48d9825571c7294667015b9c8b
Mia Pastor <mpastor@iinfo.edu.com.ar>
2026-07-28 21:33:22 -0300

    feat: arreglar formato del laboratorio 02

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 // ==========================================
@@ -8,16 +9,17 @@ declare(strict_types=1);
 
 // Desarrolla aquí tu solución según el Issue #2
 
-function calcularTotalViaticos(int $dias, float $montoDiario, float $gastosExtras): float {
-    if($dias<=0 || $montoDiario<=0 || $gastosExtras<=0){
+function calcularTotalViaticos(int $dias, float $montoDiario, float $gastosExtras): float
+{
+    if ($dias <= 0 || $montoDiario <= 0 || $gastosExtras <= 0) {
         throw new Exception("Los valores deben ser positivos.\n");
-    }else{
-        return ($dias*$montoDiario)+$gastosExtras;
+    } else {
+        return ($dias * $montoDiario) + $gastosExtras;
     }
 }
 
-try{
+try {
     echo "El total de viáticos es: $" . number_format(calcularTotalViaticos(5, 100.5, 50.75), 2) . "\n";
-}catch (Exception $e) {
+} catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
