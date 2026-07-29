d8f9a18e1d44a405000ebc9086986ba6d31a1b39
Heredia Aguilar, Rocío Edna <rheredia@iinfo.unsj.edu.ar>
2026-07-29 12:36:53 -0300

    feat: completar ejercicio del laboratorio 03

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -7,3 +7,45 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #3
+
+class MiembroEquipo{
+    private string $nombre;
+    private string $rol;
+
+    public int $tareasCompletadas;
+    
+    public function __construct(string $nombre, string $rol){
+        if (!in_array($rol, ['Junior', 'Technical Leader'])) {
+                throw new InvalidArgumentException("Rol inválido. Debe ser 'Junior' o 'Technical Leader'.");
+            }
+        $this->nombre = $nombre;
+        $this->rol = $rol;
+        $this->tareasCompletadas = 0;
+    }
+
+    public function completarTarea(): void {
+        $this->tareasCompletadas++;
+    }
+
+    public function obtenerPerfil(): string{
+        return "El miembro {$this->nombre} desempeña el rol de {$this->rol} y ha completado {$this->tareasCompletadas} laboratorios.";
+    }
+
+}
+
+
+$miembro1 = new MiembroEquipo("Ana", "Junior");
+$miembro2 = new MiembroEquipo("Juan", "Technical Leader");
+
+$miembro1->completarTarea();
+$miembro1->completarTarea();
+$miembro1->completarTarea();
+$miembro1->completarTarea();
+
+$miembro2->completarTarea();
+$miembro2->completarTarea();
+
+
+
+echo $miembro1->obtenerPerfil() . "\n";
+echo $miembro2->obtenerPerfil() . "\n";
