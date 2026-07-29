c64467134caf2ae3e9bfb6824046d0ee2e5aeabd
Micaela Agüero <maguero@iinfo.unsj.edu.ar>
2026-07-29 19:43:22 -0300

    feat: completar ejercicios del laboratorio 02

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -7,3 +7,18 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #2
+function calcularTotalViaticos(
+    int $dias,
+    float $montoDiario,
+    float $gastosExtras
+): float {
+    if ($dias <= 0 || $montoDiario <= 0 || $gastosExtras <= 0) {
+    throw new Exception("Los valores ingresados deben ser mayores a cero.");
+    }
+     return ($dias * $montoDiario) + $gastosExtras;
+}
+try {
+    echo calcularTotalViaticos(-3, 2000, 500);
+} catch (Exception $e) {
+    echo "Ocurrió un error: " . $e->getMessage();
+}
