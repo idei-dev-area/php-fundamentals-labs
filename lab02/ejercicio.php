679e562f07530cd86a2f77d189e4816f40db0fcf
jorgelloret <jlloret@iinfo.unsj.edu.ar>
2026-07-29 17:38:51 -0300

    feat: completadpo el ejercicio del laboratorio 02

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -7,3 +7,15 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #2
+function calcularTotalViaticos(int $dias, float $montoDiario, float $gastosExtras): float {
+    if ($dias <= 0 || $montoDiario <= 0 || $gastosExtras <= 0) {
+        throw new Exception("Los valores ingresados deben ser mayor a cero \n\n");
+    }
+    return ($dias * $montoDiario) + $gastosExtras;
+}
+
+try {
+    echo "Total de viáticos " . calcularTotalViaticos(3, 0, 5) . "\n\n";
+} catch (Exception $e){
+    echo "Ocurrió un error: " . $e->getMessage();
+}
