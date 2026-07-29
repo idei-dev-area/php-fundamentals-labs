3da273c8f4757dcade892f8067fcf14cee61d253
Gustavo A Sevilla <gsevilla@iinfo.unsj.edu.ar>
2026-07-29 19:51:25 -0300

    feat: implementación calcularviatico función para calcular viaticos IDEI

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -7,3 +7,15 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #2
+function calcularviatico(int $dias, float $montodiario, float $gastosExtras): float {
+    if (($dias <= 0) or ($montodiario <= 0) or ($gastosExtras <= 0)) {
+        throw new Exception("Los valores ingresados deben ser mayor que cero.");}
+    return $dias * $montodiario + $gastosExtras;
+}
+
+try {
+    echo "Total Viaticos: " . calcularviatico(7, 20.50, 100.00);
+} catch (Exception $e) {
+    echo "Ocurrió un error: " . $e->getMessage();
+}
+sleep(5);
