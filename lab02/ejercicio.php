c38ce351ca15cf5842dacb8e1e8641578304af3a
Heredia Aguilar, Rocío Edna <rheredia@iinfo.unsj.edu.ar>
2026-07-29 12:02:30 -0300

    feat: completar ejercicio del laboratorio 02

--- lab02/ejercicio.php
+++ lab02/ejercicio.php
@@ -7,3 +7,22 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #2
+function calcularTotalViaticos(int $dias, float $montoDiario, float $gastosExtras): float {
+    //Validación de parámetros
+    if ($dias <= 0 || $montoDiario <= 0 || $gastosExtras < 0) {
+        throw new Exception("Los valores ingresados deben ser mayores a cero.");
+    }
+    
+    $totalViaticos = ($dias * $montoDiario) + $gastosExtras;
+    
+    return $totalViaticos;
+}
+
+try {
+   
+    $total= calcularTotalViaticos(-6, 100.0, 50.0);
+    echo "Total de viáticos: " . $total . "\n";
+
+    } catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "\n";
+}
