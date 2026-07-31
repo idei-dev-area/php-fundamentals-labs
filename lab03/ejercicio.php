69f4ddf4203bcb7baad91fe6cf57549b4f063e30
Micaela Agüero <maguero@iinfo.unsj.edu.ar>
2026-07-31 09:34:16 -0300

    feat: completar ejercicios del laboratorio 03

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -7,3 +7,43 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #3
+class MiembroEquipo
+{
+    private string $nombre;
+    private string $rol;
+    public int $tareasCompletadas = 0;
+
+    public function __construct(string $nombre, string $rol)
+    {
+    $this->nombre = $nombre;
+    $this->rol = $rol;
+    }
+
+public function completarTarea(): void
+    {
+        $this->tareasCompletadas++;
+    }
+
+    public function obtenerPerfil(): string
+    {
+    $textoLaboratorio = "laboratorios";
+
+    if ($this->tareasCompletadas == 1) {
+        $textoLaboratorio = "laboratorio";
+    }
+
+    return "El miembro " . $this->nombre .
+            " desempeña el rol de " . $this->rol .
+            " y ha completado " . $this->tareasCompletadas .
+            " " . $textoLaboratorio . ".";
+    }
+}
+$junior = new MiembroEquipo("Micaela", "Junior");
+$tl = new MiembroEquipo("Emiliano", "Technical Leader");
+
+$junior->completarTarea();
+$junior->completarTarea();
+
+$tl->completarTarea();
+echo $junior->obtenerPerfil() . "\n";
+echo $tl->obtenerPerfil() . "\n";
