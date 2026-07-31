a3d5c3a74f28051f363f38fb5f142b3ffb7ca1d0
Sergio Dev <sgaitan@iinfo.unsj.edu.ar>
2026-07-30 21:39:18 -0300

    Se realizo el ejercicio 03

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -7,3 +7,52 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #3
+
+class MiembroEquipo
+{
+    private string $nombre;
+    private string $rol;
+
+    public int $tareasCompletas = 0;
+
+    public function __construct(string $nombre, string $rol)
+    {
+        if ($rol !== "Junior" && $rol !== "Techical Leader") {
+            throw new InvalidArgumentException ("Rol inválido. Debe ser 'Junior' o 'Technical Leader'.");
+        }
+
+        $this->nombre = $nombre;
+        $this->rol = $rol;
+    }
+
+    public function completarTareas(): void
+    {
+        $this->tareasCompletas++;
+    }
+
+    public function obtenerPerfil(): string
+    {
+        return "El miembro {$this->nombre} desempeña el rol de {$this->rol} y ha completado {$this->tareasCompletas} laboratorios.";
+    }
+}
+
+try{
+
+    $junior = new MiembroEquipo("Alejandro", "Junior");
+    $tl = new MiembroEquipo("Fabricio", "Technical Leader");
+
+    //Para forzar el error:
+    //$junior = new MiembroEquipo("Alberto", "Semi-Junior");
+    //Borrar lo comentado, para que muestre el mensaje sin error.
+
+    $junior->completarTareas();
+    $junior->completarTareas();
+
+    $tl->completarTareas();
+
+    echo $junior->obtenerPerfil() . PHP_EOL;
+    echo $tl->obtenerPerfil() . PHP_EOL;
+
+} catch (InvalidArgumentException $e) {
+    echo "Error: " . $e->getMessage();
+}
