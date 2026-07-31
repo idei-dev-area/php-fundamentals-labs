07898198d77f1daf9a41afba27e859550077bc66
jorgelloret <jlloret@iinfo.unsj.edu.ar>
2026-07-30 22:23:17 -0300

    feat: completé el laboratorio 03

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -7,3 +7,49 @@ declare(strict_types=1);
 // ==========================================
 
 // Desarrolla aquí tu solución según el Issue #3
+
+class MiembroEquipo {
+    private static array $rolesValidos = ["Junior", "Technical leader"];
+    private string $nombre;
+    private string $rol;
+    public int $tareasCompletadas = 0;
+
+    public function __construct(string $nombre, string $rol) {
+        //validación de parámetros
+        if (empty($nombre)) {
+            throw new InvalidArgumentException("El nombre no puede estar vacío");
+        }
+        if (empty($rol)) {
+            throw new InvalidArgumentException("El rol no puede estar vacío");
+        }
+
+
+        //Valores válidos: "Junior" y "Technical leader"
+        if (!in_array($rol, self::$rolesValidos)) {
+            throw new InvalidArgumentException("Rol inválido. Roles válidos; 'Junior' o 'Technical leader'." );
+            }
+        $this->nombre = $nombre;
+        $this->rol = $rol;
+    }
+    
+    public function obtenerPerfil(): string {
+        return "Nombre: {$this->nombre}, Rol: {$this->rol}, Tareas completadas: {$this->tareasCompletadas} \n";
+    }
+
+    public function completarTarea(): void {
+        $this->tareasCompletadas++;
+    }
+}
+
+$miembro1 = new MiembroEquipo("Pedro Gonzalez", "Junior");
+//$miembro2 = new MiembroEquipo("Ana Lopez", "Product owner");
+$miembro3 = new MiembroEquipo("Martin Hernandez", "Technical leader");
+
+$miembro1->completarTarea();
+//$miembro2->completarTarea();
+$miembro3->completarTarea();
+$miembro3->completarTarea();
+$miembro3->completarTarea();
+
+echo $miembro1->obtenerPerfil() . PHP_EOL;
+echo $miembro3->obtenerPerfil() . PHP_EOL;
