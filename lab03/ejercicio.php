5640c56b9dd5a14f4d027f4d716d20c8bf77a187
Facundo Gallardo <facundoggallardo@gmail.com>
2026-07-31 18:46:26 -0300

    feat: implementar clase MiembroEquipo con métodos para completar tareas y obtener perfil

--- lab03/ejercicio.php
+++ lab03/ejercicio.php
@@ -6,4 +6,44 @@ declare(strict_types=1);
 // Tema: Programación Orientada a Objetos
 // ==========================================
 
-// Desarrolla aquí tu solución según el Issue #3
+class MiembroEquipo {
+    private string $nombre;
+    private string $rol;
+    public int $tareasCompletadas;
+
+    public function __construct(string $nombre, string $rol) {
+        if ($rol !== "Junior" && $rol !== "Technical Leader") {
+            throw new Exception("El rol debe ser 'Junior' o 'Technical Leader'.\n");
+        }
+
+        $this->nombre = $nombre;
+        $this->rol = $rol;
+        $this->tareasCompletadas = 0;
+    }
+
+    public function completarTarea(): void {
+        $this->tareasCompletadas++;
+    }
+
+    public function obtenerPerfil(): string {
+        return "El miembro " . $this->nombre . " desempeña el rol de " . $this->rol . " y ha completado " . $this->tareasCompletadas . " laboratorios.\n";
+    }
+}
+
+try {
+    //Crear usuarios
+    $usuario01 = new MiembroEquipo("Florencia", "Junior");
+    $usuario02 = new MiembroEquipo("Gustavo", "Technical Leader");
+
+    //Completar tareas
+    for ($i = 0; $i < 3; $i++) {
+        $usuario01->completarTarea();
+    }
+    $usuario02->completarTarea();
+
+    //Mostrar perfiles
+    echo $usuario01->obtenerPerfil();
+    echo $usuario02->obtenerPerfil();
+} catch (Exception $e) {
+    echo "Ocurrió un error: " . $e->getMessage();
+}
